edf2f995ba0bd1cd83fe29d591f1e2fd704d8981
Héctor Francés <32682445+hfrances@users.noreply.github.com>
2022-11-14 15:03:32 +0100

    feat: implement basepath and enable environment-variables in 'appsettings.json'.

--- TusWebApplication/Startup.cs
+++ TusWebApplication/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using qckdev.Extensions.Configuration;
 using System.IO;
 using System.Text.Json.Serialization;
 using tusdotnet;
@@ -18,6 +19,7 @@ namespace TusWebApplication
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            Configuration.ApplyEnvironmentVariables();
         }
 
         public IConfiguration Configuration { get; }
@@ -48,8 +50,11 @@ namespace TusWebApplication
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var basePath = this.Configuration.GetSection("BasePath")?.Value ?? "/";
+
             app.UseCors();
 
+            app.UsePathBase(basePath);
             if (env.IsDevelopment() || env.IsStaging())
             {
                 app.UseDeveloperExceptionPage();
